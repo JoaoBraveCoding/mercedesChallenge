2e00ab64517e6250e4b677eb45458a0c63a14033
JoaoBraveCoding <joao.marcal12@hotmail.com>
2018-03-03 13:12:39 +0000

    Json is now being imported fully Have to fix the availability problem

--- booking/models.py
+++ booking/models.py
@@ -6,9 +6,11 @@ class Dealer(models.Model):
     name = models.CharField(max_length=200)
     latitude = models.IntegerField(default=0)
     longitude = models.IntegerField(default=0)
-    # vehicles = models.ForeignKey(Car, on_delete=models.CASCADE) #TODO might need to change
-    closed = models.CharField(max_length=200)  # TODO might need to change
+    closed = models.CharField(max_length=200)
 
+    def __str__(self):
+        return "id: " + self.id + "\n" + "name: " + self.name + "\n" + "location: " + str(self.latitude) + " " + str(self.longitude) + \
+    "\n" + "closed: " + self.closed
 
 class Availability(models.Model):
     key = models.CharField(max_length=200)
@@ -17,19 +19,20 @@ class Availability(models.Model):
 
 class Vehicle(models.Model):
     id = models.CharField(max_length=36, primary_key=True)
-    dealerId = models.ForeignKey(Dealer, on_delete=models.CASCADE)  # TODO might need to change
+    dealerId = models.ForeignKey(Dealer, on_delete=models.CASCADE)
     model = models.CharField(max_length=200)
     fuel = models.CharField(max_length=200)
     transmission = models.CharField(max_length=200)
     availability = models.ForeignKey(Availability, models.CASCADE)  # TODO might need to change
-
+    def __str__(self):
+        return "id: " + self.id
 
 class Booking(models.Model):
     id = models.CharField(max_length=36, primary_key=True)
     vehicleId = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
     firstName = models.CharField(max_length=200)
     lastName = models.CharField(max_length=200)
-    pickupDate = models.DateTimeField
-    createdAt = models.DateTimeField
-    canceledAt = models.DateTimeField
+    pickupDate = models.DateTimeField(null=True)
+    createdAt = models.DateTimeField(null=True)
+    canceledAt = models.DateTimeField(null=True)
     cancelledReason = models.TextField
--- scripts/script.py
+++ scripts/script.py
@@ -1,19 +1,70 @@
-#import os
 import json
+from booking.models import Dealer, Vehicle, Availability, Booking
+from datetime import datetime
 
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mercedes.settings")
-
-# your imports, e.g. Django models
-from booking.models import *
 
 
 def run():
+    # Delete db
+
+    dealers = Dealer.objects.all()
+    dealers.delete()
+    vehicles = Vehicle.objects.all()
+    vehicles.delete()
+    availabilities = Availability.objects.all()
+    availabilities.delete()
+    bookings = Booking.objects.all()
+    bookings.delete()
+
     json_data = open("datafile/dataset.json")
     data1 = json.load(json_data)
 
-    print("I was executed")
+    # Create and save dealers
+    for dealer in data1["dealers"]:
+        dealer_to_save = Dealer()
+        dealer_to_save.id = dealer["id"]
+        dealer_to_save.name = dealer["name"]
+        dealer_to_save.latitude = dealer["latitude"]
+        dealer_to_save.longitude = dealer["longitude"]
+        times = ""
+        for time in dealer["closed"]:
+            times = times + time + " "
+        dealer_to_save.closed = times.rstrip()
+        dealer_to_save.save()
+
+        #Create and save vehicles
+        for vehicle in dealer["vehicles"]:
+            vehicle_to_save =  Vehicle()
+            vehicle_to_save.id = vehicle["id"]
+            vehicle_to_save.dealerId = dealer_to_save
+            vehicle_to_save.model = vehicle["model"]
+            vehicle_to_save.fuel = vehicle["fuel"]
+            vehicle_to_save.transmission = vehicle["transmission"]
+            for availability in vehicle["availability"]:
+                availability_to_save = Availability()
+                availability_to_save.key = availability
+                times = ""
+                for time in vehicle["availability"][availability]:
+                    times = times + time + " "
+                availability_to_save.value = times.rstrip()
+                availability_to_save.save()
+            vehicle_to_save.availability = availability_to_save
+            vehicle_to_save.save()
+
+    for booking in data1["bookings"]:
+        booking_to_save = Booking()
+        booking_to_save.id = booking["id"]
+        booking_to_save.firstName = booking["firstName"]
+        booking_to_save.lastName = booking["lastName"]
+        booking_to_save.vehicleId = Vehicle.objects.get(id=booking["vehicleId"])
+        booking_to_save.pickupDate = datetime.strptime(booking["pickupDate"], '%Y-%m-%dT%H:%M:%S')
+        booking_to_save.createdAt = datetime.strptime(booking["createdAt"], '%Y-%m-%dT%H:%M:%S.%f')
+        booking_to_save.save()
+
 
     json_data.close()
+
+
 
     # e.g. add a new location
     # l = Location()
