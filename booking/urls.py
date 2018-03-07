9c2d7e5139a926ed714420358a582386a2f973a7
JoaoBraveCoding <joao.marcal12@hotmail.com>
2018-03-07 23:43:00 +0000

    Commented a bit of code and chenged urls Only missing tests and README

--- booking/urls.py
+++ booking/urls.py
@@ -3,12 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('vehicles/models', views.models, name='models'),
     path('vehicles/fuels', views.fuels, name='fuels'),
     path('vehicles/transmissions', views.transmission, name='transmissions'),
     path('dealers', views.dealer, name='dealers'),
     path('dealers/find', views.find_dealer, name='find_dealer'),
-    path('new', views.new_booking, name='new_booking'),
-    path('cancel', views.cancel_booking, name='cancel_booking')
+    path('testDrive/new', views.new_booking, name='new_booking'),
+    path('testDrive/cancel', views.cancel_booking, name='cancel_booking')
 ]
--- booking/views.py
+++ booking/views.py
@@ -8,11 +8,6 @@ import json
 import uuid
 
 
-# Create your views here.
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
-
 def models(request):
     return HttpResponse(list_by('model'))
 
@@ -28,12 +23,14 @@ def transmission(request):
 def dealer(request):
     dealers = Vehicle.objects.order_by('dealerId').values_list('dealerId', flat=True).distinct()
     json_response = {}
+
+    # Build json response
     for deal in dealers:
         json_response.setdefault(deal, [])
     for car in Vehicle.objects.all():
         json_response[car.dealerId.id].append(car.id)
-
     json_response_pretty = {'dealers': json_response}
+
     return HttpResponse(json.dumps(json_response_pretty))
 
 
@@ -41,24 +38,28 @@ def find_dealer(request):
     vehicles = Vehicle.objects.filter(model=request.META.get("HTTP_MODEL"), fuel=request.META.get("HTTP_FUEL"),
                                       transmission=request.META.get("HTTP_TRANSMISSION"))
     dealers_id = vehicles.values_list('dealerId', flat=True).distinct()
+
+    # Check if there are any dealers for the requested specs
     if len(dealers_id) == 0:
         return HttpResponse(json.dumps({"dealers": []}))
     dealers = Dealer.objects.filter(pk__in=dealers_id)
 
+    # Get user position
     user_pos = (float(request.META.get("HTTP_LATITUDE")), float(request.META.get("HTTP_LONGITUDE")))
+
+    # Create an array with every dealer and the distance to the client and sort that array
     dealers_to_return = []
     for dealer in dealers:
         current_distance = distance(user_pos, (dealer.latitude, dealer.longitude))
         dealers_to_return.append([dealer, current_distance])
-
     dealers_to_return = sorted(dealers_to_return, key=itemgetter(1))
 
+    # Build json response
     json_response = []
     for dealer in dealers_to_return:
         to_append = {"id": dealer[0].id, "name": dealer[0].name, "latitude": dealer[0].latitude,
                      "longitude": dealer[0].longitude}
         json_response.append(to_append)
-
     json_response_pretty = {"dealers": json_response}
 
     return HttpResponse(json.dumps(json_response_pretty))
@@ -70,23 +71,28 @@ def new_booking(request):
     bookings = Booking.objects.filter(vehicleId=desired_vehicle.id, canceledAt__isnull=True,
                                       cancelledReason__isnull=True)
 
+    # Parse vehicle availability weekdays and times
     vehicle_availability = desired_vehicle.availability.splitlines()
     for i in range(len(vehicle_availability)):
         vehicle_availability[i] = vehicle_availability[i].split(' ')
 
+    # Check if the vehicle has availability for requested date and time
     if not exists_availability(desired_time_slot, vehicle_availability):
         return HttpResponse("The vehicle you requested is not available at the time you requested")
 
+    # Check if there are no double booking
     if not no_double_booking(desired_time_slot, bookings):
         return HttpResponse("There is already a test drive for that time and vehicle")
 
+    # Create and save the booking
     booking_to_save = Booking.objects.create(id=uuid.uuid4(), vehicleId=desired_vehicle,
                                              firstName=request.META.get("HTTP_FIRSTNAME"),
                                              lastName=request.META.get("HTTP_LASTNAME"),
                                              pickupDate=desired_time_slot,
                                              createdAt=datetime.now())
     booking_to_save.save()
-    return HttpResponse("Success")
+
+    return HttpResponse("Test drive booked success")
 
 
 def cancel_booking(request):
@@ -94,34 +100,21 @@ def cancel_booking(request):
     booking_to_cancel.cancelledReason = request.META.get("HTTP_CANCELLEDREASON")
     booking_to_cancel.canceledAt = datetime.now()
     booking_to_cancel.save()
-    return HttpResponse("Booking canceled successfully")
 
-
-def no_double_booking(desired_time_slot, bookings):
-    for booking in bookings:
-        if desired_time_slot == booking.pickupDate:
-            return False
-    return True
-
-
-def exists_availability(desired_time_slot, vehicle_availability):
-    for i in range(len(vehicle_availability)):
-        if vehicle_availability[i][0] == desired_time_slot.strftime('%A').lower():
-            for j in range(1, len(vehicle_availability[i])):
-                if desired_time_slot.strftime('%H%M') == vehicle_availability[i][j]:
-                    return True
-    return False
+    return HttpResponse("Test drive canceled successfully")
 
 
 def list_by(attribute):
     attribute_types = Vehicle.objects.order_by(attribute).values_list(attribute, flat=True).distinct()
+
+    # Build json response
     json_response = {}
     for atr in attribute_types:
         json_response.setdefault(atr, [])
     for car in Vehicle.objects.all():
         json_response[car.__getattribute__(attribute)].append(car.id)
-
     json_response_pretty = {attribute + 's': json_response}
+
     return HttpResponse(json.dumps(json_response_pretty))
 
 
@@ -137,3 +130,19 @@ def distance(origin, destination):
          math.sin(d_lon / 2) * math.sin(d_lon / 2))
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
     return radius * c
+
+
+def exists_availability(desired_time_slot, vehicle_availability):
+    for i in range(len(vehicle_availability)):
+        if vehicle_availability[i][0] == desired_time_slot.strftime('%A').lower():
+            for j in range(1, len(vehicle_availability[i])):
+                if desired_time_slot.strftime('%H%M') == vehicle_availability[i][j]:
+                    return True
+    return False
+
+
+def no_double_booking(desired_time_slot, bookings):
+    for booking in bookings:
+        if desired_time_slot == booking.pickupDate:
+            return False
+    return True
